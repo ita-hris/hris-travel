72a04dfb389cb10736a7916cdf6b29241840b6f8
maximVorochaev <maxim.vorochaev@mail.ru>
2020-01-23 11:58:27 +0300

    Initial commit

---
+++ src/main/java/com/itechart/hris/hristravel/HrisTravelApplication.java
@@ -0,0 +1,13 @@
+package com.itechart.hris.hristravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrisTravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrisTravelApplication.class, args);
+    }
+
+}
