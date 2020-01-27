f296b020208171aa0491fef35915c489524afa47
Artem Matveev <magicsaros@gmail.com>
2020-01-27 16:06:17 +0300

    Edit dozer config

--- src/main/java/com/itechart/hris/hristravel/config/AppConfig.java
+++ src/main/java/com/itechart/hris/hristravel/config/AppConfig.java
@@ -3,16 +3,20 @@ package com.itechart.hris.hristravel.config;
 import java.util.List;
 import org.dozer.DozerBeanMapper;
 import org.dozer.Mapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
 
+    @Value("${dozer.file.path}")
+    private String dozerFilePath;
+
     @Bean
     public Mapper dozerBeanMapper() {
         DozerBeanMapper mapper = new DozerBeanMapper();
-        mapper.setMappingFiles(List.of("dozer-bean-mappings.xml"));
+        mapper.setMappingFiles(List.of(dozerFilePath));
         return mapper;
     }
 }
