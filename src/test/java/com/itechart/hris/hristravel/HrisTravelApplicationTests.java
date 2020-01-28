255599db68b951c7af3300bdfd7247916db5f299
maxim <maxim.vorochaev@mail.ru>
2020-01-28 12:32:13 +0300

    Init logging

--- src/test/java/com/itechart/hris/hristravel/HrisTravelApplicationTests.java
+++ src/test/java/com/itechart/hris/hristravel/HrisTravelApplicationTests.java
@@ -1,13 +1,13 @@
-package com.itechart.hris.hristravel;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class HrisTravelApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.itechart.hris.hristravel;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class HrisTravelApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
