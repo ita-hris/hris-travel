d53c39cbcaa5558352457482bf8201fd40bf1912
Pavel Yeudakimovich <pavel.yeudakimovich@itechart-group.com>
2020-01-29 14:04:33 +0300

    Bugfix corporate model

--- src/main/java/com/itechart/hris/hristravel/model/entity/IdHolder.java
+++ src/main/java/com/itechart/hris/hristravel/model/entity/IdHolder.java
@@ -4,7 +4,7 @@ public interface IdHolder {
 
     String DEFAULT_ID_FIELD_NAME = "id";
 
-    Integer getId();
+    Long getId();
 
     static String getIdFieldName() {
         return DEFAULT_ID_FIELD_NAME;
