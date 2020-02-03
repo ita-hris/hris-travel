8c7b98c2d81027113ebc90c54b7147cb85bfb572
Artem Matveev <magicsaros@gmail.com>
2020-02-03 14:19:35 +0300

    Add api documentation tags for controllers

--- src/main/java/com/itechart/hris/hristravel/web/controller/VisaRequestController.java
+++ src/main/java/com/itechart/hris/hristravel/web/controller/VisaRequestController.java
@@ -3,11 +3,13 @@ package com.itechart.hris.hristravel.web.controller;
 import com.itechart.hris.hristravel.model.entity.VisaRequest;
 import com.itechart.hris.hristravel.service.VisaRequestService;
 import com.itechart.hris.hristravel.web.controller.common.AbstractController;
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/visas")
+@Api(tags = {"Visa request API"})
 public class VisaRequestController extends AbstractController<VisaRequest, VisaRequestService> {
 
     protected VisaRequestController(VisaRequestService service) {
